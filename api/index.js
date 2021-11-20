c9029f576dadf53365eba40d518be47582398fd1
rizky-aaditya <57607397+rizky-aaditya@users.noreply.github.com>
2021-11-20 13:46:28 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = 'your_telegram_token';
+const token = '2115368098:AAHNBV8xA6U8tkzjR0LjgNa44s-mGlz_PUE';
 const bot = new TelegramBot(token, {polling: true});
 
 
